e99d28a929bbb309c356094cbe92ee60208ffe27
= <->
2019-05-13 21:59:11 +0200

    Added checking game api

--- models/game.js
+++ models/game.js
@@ -1,58 +1,77 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
-const Game = mongoose.model('Game', new mongoose.Schema({
-
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const Game = mongoose.model(
+  "Game",
+  new mongoose.Schema({
     gameId: {
-        type: String,
+      type: String
     },
     player: {
-        type: String,
-        minlength: 3,
-        maxlength: 50,
-        required: true,
-        default: "Anonymous Player",
+      type: String,
+      minlength: 3,
+      maxlength: 50,
+      required: true,
+      default: "Anonymous Player"
     },
     quoteId: {
-        type: String,
-        lowercase: true,
-        trim: true,
+      type: String,
+      lowercase: true,
+      trim: true
     },
-    difficulty :{
-        type: String,
-        enum: ['easy','medium','hard']
+    difficulty: {
+      type: String,
+      enum: ["easy", "medium", "hard"]
     },
     quoteLength: {
-        type: Number,
-        
+      type: Number
     },
-    letterToGuess :{
-        type: Number,
+    letterToGuess: {
+      type: Number
     },
-    lang:{
-        type: String,
+    sendedLetters: {
+      type: Array
     },
-    guessedLetters:{
-        type: Number,
+    lang: {
+      type: String
     },
-    lifes:{
-        type: String,
+    guessedLetters: {
+      type: Number
+    },
+    lifes: {
+      type: String
     }
-}))
+  })
+);
 
 function validateGame(reqNewGame) {
-    const schema = {
-        player: Joi.string().min(3).max(50).required(),
-        level: Joi.string().min(3).max(10).required(),
-        lang: Joi.string().min(3).max(15).required(),
-    };
-    return Joi.validate(reqNewGame, schema);
+  const schema = {
+    player: Joi.string()
+      .min(3)
+      .max(50)
+      .required(),
+    level: Joi.string()
+      .min(3)
+      .max(10)
+      .required(),
+    lang: Joi.string()
+      .min(3)
+      .max(15)
+      .required()
+  };
+  return Joi.validate(reqNewGame, schema);
 }
 function validateCheck(reqCheckGame) {
-    const schema = {
-        letter: Joi.string().min(1).max(1).required(),
-        gameId: Joi.string().min(1).max(255).required(),
-    };
-    return Joi.validate(reqCheckGame, schema);
+  const schema = {
+    letter: Joi.string()
+      .min(1)
+      .max(1)
+      .required(),
+    gameId: Joi.string()
+      .min(1)
+      .max(255)
+      .required()
+  };
+  return Joi.validate(reqCheckGame, schema);
 }
 
 module.exports.Game = Game;
