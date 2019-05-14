031c7250d112703ea170791a8e39e908a5d3b15e
= <->
2019-05-14 23:45:29 +0200

    css changes

--- routes/home.js
+++ routes/home.js
@@ -9,7 +9,7 @@ router.get("/", (req, res) => {
 router.get("/SysInfo", (req, res) => {
   res.render("sysinfo", {
     title: site.title,
-    urlToFetch: "//" + req.get("host") + "/api/sysinfo"
+    urlToFetch: "//" + req.get("host") + "/api/sysinfo" // without req.protocol
   });
 });
 router.get("/About", (req, res) => {
