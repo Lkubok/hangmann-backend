619b0f09c3a0ad0cb9ac94b5c522716de47a1047
= <->
2019-05-14 22:40:06 +0200

    added /api/quotes/add for adding the quotes to DB

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,5 +1,5 @@
 const { Quote, validate } = require("../models/quote");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 const randomize = require("../functions/randomize");
@@ -9,6 +9,7 @@ const randomize = require("../functions/randomize");
 // const asyncMiddleware = require('../middleware/async');      // Function to Add in Future
 
 /* START OF GET HTTP ACTIONS */
+// /api/quotes - returns all quotes in DB
 router.get("/", async (req, res) => {
   const quotes = await Quote.find().sort("dateInsert");
   res.send(quotes);
@@ -43,4 +44,45 @@ router.get(`/random`, async (req, res) => {
   res.send(randomQuote);
 });
 /* END OF GET ACTIONS */
+/* POST ACTIONS START */
+
+router.post(`/add`, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const quotes = await Quote.find().sort("dateInsert");
+  let quotesArray = [];
+  quotes.map(element => {
+    //Making an array of quotes to compare with given quote from browser
+    quotesArray.push(element.quote);
+  });
+  if (quotesArray.includes(req.body.quote))
+    // Searching if quote is already in Database
+    return res.send({
+      status: "rejected",
+      message: "Quote already in Database"
+    });
+  const quoteToInsert = req.body.quote.toLowerCase();
+  /* REFACTOR CODE HERE */
+  function writeDifficulty(str) {
+    // Export this function in future
+    if (str.length < 40) {
+      return "easy";
+    } else if (str.length < 80) {
+      return "medium";
+    } else {
+      return "hard";
+    }
+  }
+  let newQuote = new Quote({
+    quoteAuthor: req.body.quoteAuthor,
+    insertAuthor: req.body.insertAuthor,
+    quote: quoteToInsert,
+    lang: req.body.lang,
+    difficulty: writeDifficulty(quoteToInsert)
+  });
+  newQuote = await newQuote.save();
+  res.send(newQuote);
+});
+
+/* END OF POST ACTIONS */
 module.exports = router;
