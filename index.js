9439f4eecd3e3ff5ae1669a6a4ebb28e1e957f47
ŁukaszKubok <syrathea@gmail.com>
2019-07-08 21:37:08 +0200

    Added CORS Policy

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-require('dotenv').config({ path: './config/variables.env' });       //Configure system variables, change variables when DEPLOY!!
+require("dotenv").config({ path: "./config/variables.env" }); //Configure system variables, change variables when DEPLOY!!
+const express = require("express");
+const app = express();
+let allowCrossDomain = require("./startup/allowCrossDomain");
+app.use(allowCrossDomain);
+const winston = require("winston");
 
-const express = require('express');
-const app = express();  
-const winston = require('winston');
+require("./startup/logging")(); //it must be first
+require("./startup/database")();
+require("./startup/routes")(app);
 
-require('./startup/logging')();  //it must be first
-require('./startup/database')(); 
-require('./startup/routes')(app);
+app.set("view engine", "pug"); //Setting a html template engine, node will automaticly load the pug module
+app.set("views", "./views"); //This is the same as default
 
-app.set('view engine', 'pug');              //Setting a html template engine, node will automaticly load the pug module
-app.set('views','./views');                 //This is the same as default
+const port = process.env.PORT || process.env.SERV_PORT; // SERV_PORT is my own varaible defined to work with front-end on one environment
 
-const port = process.env.PORT || process.env.SERV_PORT;     // SERV_PORT is my own varaible defined to work with front-end on one environment
-
-app.listen(port, ()=>{
-    winston.info(`Listening on port ${port}`);                          // Displaying port
-    winston.info(`Running in environment: ${app.get('env')}`)           // Displaying ENV
-    winston.info(`Private server Key: ${process.env.SERV_JWT}`)  //DELETE       // Displaying jwtPrivateKey - Delete this in FUTURE, only for debug!!
-    winston.info(`Mongo URL: ${process.env.MONGO_URL}`)          //DELETE       // Displaying URL to Database - Delete this in FUTURE, only for debug!!
+app.listen(port, () => {
+  winston.info(`Listening on port ${port}`); // Displaying port
+  winston.info(`Running in environment: ${app.get("env")}`); // Displaying ENV
+  winston.info(`Private server Key: ${process.env.SERV_JWT}`); //DELETE       // Displaying jwtPrivateKey - Delete this in FUTURE, only for debug!!
+  winston.info(`Mongo URL: ${process.env.MONGO_URL}`); //DELETE       // Displaying URL to Database - Delete this in FUTURE, only for debug!!
 });
